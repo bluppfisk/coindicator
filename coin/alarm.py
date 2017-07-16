c490ec88588aaac9a8dc1010795f264030cc8395
arnau-foraster <arnau.foraster@gmail.com>
2017-07-16 17:15:03 +0200

    Cosmetic changes and styling (#6)

--- coin/alarm.py
+++ coin/alarm.py
@@ -2,32 +2,34 @@
 
 # Alarm
 
-__author__ = "nil.gradisnik@gmail.com"
-
 import notify2 as notify
 
-class Alarm():
+__author__ = "nil.gradisnik@gmail.com"
 
-  def __init__(self, app_name, ceil=1000, floor=100):
-    self.app_name = app_name
-    self.ceil = ceil
-    self.floor = floor
 
-  def setCeil(self, price):
-    self.ceil = price
+class Alarm(object):
+    def __init__(self, app_name, ceil=1000, floor=100):
+        self.app_name = app_name
+        self.ceil = ceil
+        self.floor = floor
 
-  def setFloor(self, price):
-    self.floor = price
+    def set_ceil(self, price):
+        self.ceil = price
 
-  def check(self, price):
-    if self.ceil <= price:
-      self.__notify('High Bitcoin price alert: $' + str(price), 'Current bitcoin price rose above your alarm threshold: $' + str(self.ceil))
-    if self.floor >= price:
-      self.__notify('Low Bitcoin price alert: $' + str(price), 'Current bitcoin price fell below your alarm threshold: $' + str(self.floor))
+    def set_floor(self, price):
+        self.floor = price
 
-  def __notify(self, title, message):
-    if notify.init(self.app_name):
-      n = notify.Notification(title, message)
-      n.set_urgency(notify.URGENCY_CRITICAL)
-      n.set_timeout(notify.EXPIRES_NEVER)
-      n.show()
+    def check(self, price):
+        if self.ceil <= price:
+            self.__notify('High Bitcoin price alert: $' + str(price),
+                          'Current bitcoin price rose above your alarm threshold: $' + str(self.ceil))
+        if self.floor >= price:
+            self.__notify('Low Bitcoin price alert: $' + str(price),
+                          'Current bitcoin price fell below your alarm threshold: $' + str(self.floor))
+
+    def __notify(self, title, message):
+        if notify.init(self.app_name):
+            n = notify.Notification(title, message)
+            n.set_urgency(notify.URGENCY_CRITICAL)
+            n.set_timeout(notify.EXPIRES_NEVER)
+            n.show()
--- coin/utils.py
+++ coin/utils.py
@@ -5,17 +5,18 @@
 __author__ = "nil.gradisnik@gmail.com"
 
 currency = {
-  'usd': '$',
-  'eur': '€'
+    'usd': '$',
+    'eur': '€'
 }
 
 category = {
-  'bid': 'Bid:\t\t',
-  'high': 'High:\t\t',
-  'low': 'Low:\t\t',
-  'ask': 'Ask:\t\t',
-  'volume': 'Volume:\t'
+    'bid': 'Bid:\t\t',
+    'high': 'High:\t\t',
+    'low': 'Low:\t\t',
+    'ask': 'Ask:\t\t',
+    'volume': 'Volume:\t'
 }
 
+
 def decimal_round(number):
-  return "%.2f" % float(number)
+    return "%.2f" % float(number)
