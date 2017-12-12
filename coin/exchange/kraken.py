ddff4c04a81b06afd0201d9b598fd490a1e1111c
bluppfisk <sander.vandemoortel@gmail.com>
2017-12-12 20:00:46 +0800

    seems to fix UI snafus on Kraken

--- coin/exchange/kraken.py
+++ coin/exchange/kraken.py
@@ -8,6 +8,7 @@ __author__ = "nil.gradisnik@gmail.com"
 from gi.repository import GLib
 
 import requests
+from threading import Thread
 import logging
 
 import utils
@@ -42,7 +43,7 @@ CONFIG = {
       'currency': utils.currency['eur']
     },
     {
-      'isocode': 'XETHZUSD',
+      'isocode': 'XXETZUSD',
       'pair': 'XETHZUSD',
       'name': 'ETH to USD',
       'currency': utils.currency['usd']
@@ -80,7 +81,7 @@ CONFIG = {
   ]
 }
 
-class Kraken:
+class Kraken(object):
 
   def __init__(self, config, indicator):
     self.indicator = indicator
@@ -91,6 +92,7 @@ class Kraken:
     self.error = Error(self)
 
   def start(self, error_refresh=None):
+    self.indicator.idle()
     refresh = error_refresh if error_refresh else self.indicator.refresh_frequency
     self.timeout_id = GLib.timeout_add_seconds(refresh, self.check_price)
 
@@ -98,28 +100,46 @@ class Kraken:
     if self.timeout_id:
       GLib.source_remove(self.timeout_id)
 
+    if hasattr(self, 'thread'):
+      self.thread.join()
+
   def check_price(self):
     self.asset_pair = self.indicator.active_asset_pair
 
     self.pair = [item['pair'] for item in CONFIG['asset_pairs'] if item['isocode'] == self.asset_pair][0]
 
-    try:
-      res = requests.get(CONFIG['ticker'] + '?pair=' + self.pair)
-      data = res.json()
-      if data['error']:
-        self._handle_error(data['error'])
-      elif data['result']:
-        self._parse_result(data['result'])
+    self.async_get(CONFIG['ticker'] + '?pair=' + self.pair, callback=self._parse_result)
 
-    except Exception as e:
-      logging.info('Error: ' + str(e))
-      self.error.increment()
+  def async_get(self, *args, callback=None, timeout=15, **kwargs):
+    """Makes request on a different thread, and optionally passes response to a
+    `callback` function when request returns.
+    """
+    if callback:
+        def callback_with_args(response, *args, **kwargs):
+            callback(response)
+        kwargs['hooks'] = {'response': callback_with_args}
+    kwargs['timeout'] = timeout
+    self.thread = Thread(target=requests.get, args=args, kwargs=kwargs)
+    self.thread.start()
+    # try:
+    #   res = requests.get(CONFIG['ticker'] + '?pair=' + self.pair)
+    #   data = res.json()
+    #   if data['error']:
+    #     self._handle_error(data['error'])
+    #   elif data['result']:
+    #     self._parse_result(data['result'])
 
-    return self.error.is_ok()
+    # except Exception as e:
+    #   logging.info('Error: ' + str(e))
+    #   self.error.increment()
+
+    # return self.error.is_ok()
+    # 
 
   def _parse_result(self, data):
     self.error.clear()
-
+    print(data.json())
+    data = data.json()['result']
     asset = data[self.pair]
     currency = [item['currency'] for item in CONFIG['asset_pairs'] if item['isocode'] == self.asset_pair][0]
     coin = [item['name'] for item in CONFIG['asset_pairs'] if item['isocode'] == self.asset_pair][0]
