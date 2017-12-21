57e5e5853d57f916f12fe200cde6e1a7c9b26e0d
bluppfisk <sander.vandemoortel@gmail.com>
2017-12-21 18:30:27 +0800

    working toward #28

--- coin/exchange.py
+++ coin/exchange.py
@@ -28,6 +28,7 @@ class Exchange(object):
     self.error = Error(self)
     self.config = self.CONFIG
     self.exchange_name = self.config['name']
+    self.started = False
 
   def get_ticker(self): # to be overwritten by child class
     pass
@@ -36,19 +37,27 @@ class Exchange(object):
     pass
 
   def start(self, error_refresh=None):
+    if not self.started:
+      self._check_price()
+
+    self.started = True
     refresh = error_refresh if error_refresh else self.indicator.refresh_frequency
-    logging.info("loading " + self.asset_pair + " from " + self.exchange_name + " (" + str(refresh) + "s)")
-    self.timeout_id = GLib.timeout_add_seconds(refresh, self.check_price)
+    self.timeout_id = GLib.timeout_add_seconds(refresh, self._check_price)
+
+    return self
 
   def stop(self):
+    self.started = False
     self.error.clear()
     if self.timeout_id is not None:
         GLib.source_remove(self.timeout_id)
 
-  def check_price(self):
+    return self
+
+  def _check_price(self):
     self.asset_pair = self.indicator.active_asset_pair
     self.pair = [item.get('pair') for item in self.config.get('asset_pairs') if item.get('isocode') == self.asset_pair][0]
-    self.async_get(self.get_ticker(), validation=self.asset_pair, callback=self._handle_result)
+    self._async_get(self.get_ticker(), validation=self.asset_pair, callback=self._handle_result)
 
     return self.error.is_ok() # continues the timer if there are no errors
 
@@ -64,7 +73,7 @@ class Exchange(object):
 
     # also check if a newer response hasn't already been returned
     if timestamp < self.latest_response: # this is an older request
-      logging.warning('Discarding outdated response.')
+      logging.info('Discarding outdated response.')
       return
 
     if data.status_code != 200:
@@ -87,20 +96,20 @@ class Exchange(object):
     currency = config['currency']
     volumecurrency = config.get('volumelabel').upper() if config.get('volumelabel') else config.get('name')[0:3].upper()
 
-    label = currency + self.decimal_auto(results.get('label'))
-    bid = CATEGORY['bid'] + ':\t\t' + currency + self.decimal_auto(results.get('bid')) if results.get('bid') else None
-    high = CATEGORY['high'] + ':\t\t' + currency + self.decimal_auto(results.get('high')) if results.get('high') else None
-    low = CATEGORY['low'] + ':\t\t' + currency + self.decimal_auto(results.get('low')) if results.get('low') else None
-    ask = CATEGORY['ask'] + ':\t\t' + currency + self.decimal_auto(results.get('ask')) if results.get('ask') else None
-    vol = CATEGORY['volume'] + ' (' + volumecurrency + '):\t' + self.decimal_auto(results.get('vol')) if results.get('vol') else None
+    label = currency + self._decimal_auto(results.get('label'))
+    bid = CATEGORY['bid'] + ':\t\t' + currency + self._decimal_auto(results.get('bid')) if results.get('bid') else None
+    high = CATEGORY['high'] + ':\t\t' + currency + self._decimal_auto(results.get('high')) if results.get('high') else None
+    low = CATEGORY['low'] + ':\t\t' + currency + self._decimal_auto(results.get('low')) if results.get('low') else None
+    ask = CATEGORY['ask'] + ':\t\t' + currency + self._decimal_auto(results.get('ask')) if results.get('ask') else None
+    vol = CATEGORY['volume'] + ' (' + volumecurrency + '):\t' + self._decimal_auto(results.get('vol')) if results.get('vol') else None
     
     GLib.idle_add(self.indicator.set_data, label, bid, high, low, ask, vol)
 
-  ## decimal_auto 
+  ## _decimal_auto 
   # Rounds a number to a meaningful number of decimal places
   # and returns it as a string
   # 
-  def decimal_auto(self, number):
+  def _decimal_auto(self, number):
     number = float(number)
     if number < 10:
         for i in range(8, 0, -1):
@@ -117,17 +126,17 @@ class Exchange(object):
   # Makes request on a different thread, and optionally passes response to a
   # `callback` function when request returns.
   # 
-  def async_get(self, *args, callback=None, timeout=15, validation=None, **kwargs):  
+  def _async_get(self, *args, callback=None, timeout=15, validation=None, **kwargs):  
     if callback:
-      def callback_with_args(response, *args, **kwargs):
+      def _callback_with_args(response, *args, **kwargs):
         timestamp = time.time()
         callback(response, validation, timestamp)
-      kwargs['hooks'] = {'response': callback_with_args}
+      kwargs['hooks'] = {'response': _callback_with_args}
     kwargs['timeout'] = timeout
-    thread = Thread(target=self.get_with_exception, args=args, kwargs=kwargs)
+    thread = Thread(target=self._get_with_exception, args=args, kwargs=kwargs)
     thread.start()
 
-  def get_with_exception(self, *args, **kwargs):
+  def _get_with_exception(self, *args, **kwargs):
     try:
         r = requests.get(*args, **kwargs)
         return r
