e5423092a9821052dda983763b82bd6ae3219a32
Sander Van de Moortel <sander.vandemoortel@gmail.com>
2020-04-08 01:00:20 +0200

    simplifies install procedure allowing system-wide packages

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get install libcairo2-dev libdbus-glib-1-2 gir1.2-gtk-3.0 gir1.2-appindicator3-0.1
-python3 -m venv ./
+sudo apt-get install libdbus-glib-1-2 gir1.2-gtk-3.0 gir1.2-appindicator3-0.1
+python3 -m venv --system-site-packages ./
 source ./bin/activate
 pip3 install -r requirements.txt
-vext -i gi.vext
+# vext -i gi.vext
 chmod u+x coin/coin.py
 deactivate
