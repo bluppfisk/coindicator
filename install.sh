7fed9a4561dedff210db6a3e621c4618a38863aa
Sander Van de Moortel <sander.vandemoortel@gmail.com>
2020-04-16 02:57:13 +0200

    adds python3-venv to installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install libcairo2-dev libdbus-glib-1-2 gir1.2-gtk-3.0 gir1.2-appindicator3-0.1
+sudo apt-get install python3-venv libcairo2-dev libdbus-glib-1-2 gir1.2-gtk-3.0 gir1.2-appindicator3-0.1
 python3 -m venv ./
 source ./bin/activate
 pip3 install -r requirements.txt
