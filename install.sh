b0247406de98278955d223ce195e2737a1460c0d
Sander Van de Moortel <sander.vandemoortel@gmail.com>
2020-05-03 21:10:18 +0200

    provides launcher shortcut

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Install system dependencies
 sudo apt-get install python3-venv python3-pygame libcairo2-dev libdbus-glib-1-2 gir1.2-gtk-3.0 gir1.2-appindicator3-0.1 -y
+# Install environment
 python3 -m venv --system-site-packages ./
 source ./bin/activate
+# Install python packages
 pip3 install -r requirements.txt
 chmod u+x coin/coin.py
 deactivate
+# Install shortcut
+echo Exec=`pwd`/run.sh >> coindicator.desktop
+echo Icon=`pwd`/resources/logo_248px.png >> coindicator.desktop
+desktop-file-install --dir=$HOME/.local/share/applications coindicator.desktop
