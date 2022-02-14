326c4f505ad87d14b27396bb00d63b263bcccca8
thesolarminer <thesolarminer@users.noreply.github.com>
2022-02-14 13:20:25 -0600

    python3-pip needed for Ubuntu22

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install system dependencies
-sudo apt-get install python3-venv libcairo2-dev libdbus-glib-1-2 gir1.2-gtk-3.0 gir1.2-appindicator3-0.1 -y
+sudo apt-get install python3-venv libcairo2-dev libdbus-glib-1-2 gir1.2-gtk-3.0 gir1.2-appindicator3-0.1 python3-pip -y
 pip3 install pygame
 # Install environment
 python3 -m venv --system-site-packages ./
