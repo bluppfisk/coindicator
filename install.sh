6b2a995e69da1eb8fdec8235b83432dadf27ce07
Sander Van de Moortel <sander.vandemoortel@gmail.com>
2022-10-30 20:14:23 +0100

    updates to install script

--- install.sh
+++ install.sh
@@ -3,10 +3,23 @@
 sudo apt-get install python3-venv python3-gi python3-gi-cairo gir1.2-gtk-3.0 libdbus-glib-1-2 gir1.2-appindicator3-0.1 python3-pip -y
 
 # Install python packages
-pip3 install --user .
+pip3 install -U coindicator
 
 # Install shortcut
-echo Path=`pwd` >> coindicator.desktop
-echo Exec=coin >> coindicator.desktop
-echo Icon=`pwd`/src/coin/resources/logo_248px.png >> coindicator.desktop
+cat > coindicator.desktop << EOL
+
+[Desktop Entry]
+Name=Coindicator
+GenericName=Cryptocoin price ticker
+Comment=Keep track of the cryptocoin prices on various exchanges
+Terminal=false
+Type=Application
+Categories=Utility;Network;
+Keywords=crypto;coin;ticker;price;exchange;
+StartupNotify=false
+Path=`pwd`
+Exec=coin
+Icon=`pwd`/src/coin/resources/logo_248px.png
+EOL
+
 desktop-file-install --dir=$HOME/.local/share/applications coindicator.desktop
