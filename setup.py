f97c1d53c2353b1945214437f705af7494e70599
Sander Van de Moortel <sander.vandemoortel@gmail.com>
2023-06-16 14:59:12 +0200

    2.1.3

--- setup.py
+++ setup.py
@@ -4,10 +4,7 @@ setup_requirements = ["setuptools_scm[toml]>=5", "setuptools>=46.1.0"]
 
 if __name__ == "__main__":
     try:
-        setup(
-            use_scm_version={"version_scheme": "no-guess-dev"},
-            requires=setup_requirements,
-        )
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
