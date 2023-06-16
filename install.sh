18a22c81ec100d3685a24d2533bb009fa4147fa2
Sander Van de Moortel <sander.vandemoortel@gmail.com>
2023-06-16 17:26:57 +0200

    remove use deprecated

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ sudo apt-get install python3-venv python3-setuptools-scm python3-wheel python3-g
 sudo mkdir ${args[0]} 2>/dev/null
 sudo python3 -m venv ${args[0]}/venv
 source ${args[0]}/venv/bin/activate
-sudo -H -E env PATH=$PATH pip3 install --no-cache-dir --use-deprecated=legacy-resolver -U coindicator
+sudo -H -E env PATH=$PATH pip3 install -U coindicator
 
 # Install shortcut
 cat > /tmp/coindicator.desktop << EOL
